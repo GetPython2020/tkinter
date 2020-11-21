38f735054888ae3e57c43fd9fd29f6165aea1476
GetPython2020 <72427707+GetPython2020@users.noreply.github.com>
2020-11-20 17:04:39 -0700

    Update grid_geometry.py

--- grid_geometry.py
+++ grid_geometry.py
@@ -7,7 +7,7 @@ for i in range(3):
     window.rowconfigure(i, weight=1, minsize=50)
     for j in range(3):
         frame = tk.Frame(master=window,relief=tk.RAISED,borderwidth=1)
-        frame.grid(row=i,column=j,padx=5,pady=5)
+        frame.grid(row=i,column=j,padx=5,pady=5)  # padx:horizontal padding, pady:vertical padding
         label=tk.Label(master=frame,text=f'row{i},column{j}')
         label.pack(padx=40,pady=10)
 
